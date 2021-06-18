327890590b2211f8f10ad5d7475b2036dcd08352
Senthil <senthil.jegadeesan@embitel.com>
2021-06-19 00:25:28 +0530

    basic layout

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './themes/styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Go For Gold</p>
+
+        <a href="#" class="btn btn-primary">Green Button</a>
+        <a href="#" class="btn btn-secondary">Blue Button</a>
+        <a href="#" class="btn btn-orange">Yellow Button</a>
+    </div>
+  );
+}
+
+export default App;
