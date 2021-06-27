cb5bac963f4101224be1720c895f32256c10cbb5
Senthil <senthil.jegadeesan@embitel.com>
2021-06-28 00:48:40 +0530

    dropdown

--- src/components/StepFour.tsx
+++ src/components/StepFour.tsx
@@ -3,47 +3,33 @@ import React from 'react';
 const StepFour = () => {
     return (
         <div className="survey-container">
+            
             <div className="survey-card">
                 <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-right">Less Than One Acre Step</span>
+                    <span className="survey-right">Kharif / Food Grains</span>
                     <input type="radio"  name="survey-step-1" />
                     <span className="checkmark"></span>
                 </label>
+                <div className="survey-dropdown">
+                    <select>
+                        <option>Rice</option>
+                        <option>Wheat</option>
+                    </select>
+                </div>
             </div>
 
             <div className="survey-card active">
                 <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-left">1 - 5</span>
-                    <span className="survey-right">Acre</span>
+                    <span className="survey-right">Cash Crops / Non - Food Crops</span>
                     <input type="radio" name="survey-step-1" checked/>
                     <span className="checkmark"></span>
                 </label>
-            </div>
-
-            <div className="survey-card">
-                <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-left">5 - 10</span>
-                    <span className="survey-right">Acre</span>
-                    <input type="radio"  name="survey-step-1" />
-                    <span className="checkmark"></span>
-                </label>
-            </div>
-
-            <div className="survey-card">
-                <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-left">10</span>
-                    <span className="survey-right">Acre</span>
-                    <input type="radio"  name="survey-step-1" />
-                    <span className="checkmark"></span>
-                </label>
-            </div>
-
-            <div className="survey-card">
-                <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-right">More than Ten Acre</span>
-                    <input type="radio"  name="survey-step-1" />
-                    <span className="checkmark"></span>
-                </label>
+                <div className="survey-dropdown">
+                    <select>
+                        <option>Sugarcane</option>
+                        <option>Sugarcane</option>
+                    </select>
+                </div>
             </div>
 
         </div>
--- src/components/StepThree.tsx
+++ src/components/StepThree.tsx
@@ -5,7 +5,7 @@ const StepThree = () => {
         <div className="survey-container">
             <div className="survey-card">
                 <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-right">Less Than One Acre Step</span>
+                    <span className="survey-right">Plains</span>
                     <input type="radio"  name="survey-step-1" />
                     <span className="checkmark"></span>
                 </label>
@@ -13,8 +13,7 @@ const StepThree = () => {
 
             <div className="survey-card active">
                 <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-left">1 - 5</span>
-                    <span className="survey-right">Acre</span>
+                    <span className="survey-right">Hills</span>
                     <input type="radio" name="survey-step-1" checked/>
                     <span className="checkmark"></span>
                 </label>
@@ -22,8 +21,7 @@ const StepThree = () => {
 
             <div className="survey-card">
                 <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-left">5 - 10</span>
-                    <span className="survey-right">Acre</span>
+                    <span className="survey-right">Mountains</span>
                     <input type="radio"  name="survey-step-1" />
                     <span className="checkmark"></span>
                 </label>
@@ -31,21 +29,11 @@ const StepThree = () => {
 
             <div className="survey-card">
                 <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-left">10</span>
-                    <span className="survey-right">Acre</span>
+                    <span className="survey-right">Mixed</span>
                     <input type="radio"  name="survey-step-1" />
                     <span className="checkmark"></span>
                 </label>
             </div>
-
-            <div className="survey-card">
-                <label className="survey-card-label gfg-input input-radio">
-                    <span className="survey-right">More than Ten Acre</span>
-                    <input type="radio"  name="survey-step-1" />
-                    <span className="checkmark"></span>
-                </label>
-            </div>
-
         </div>
     );
 }
